56d1889f725947291a660c668c023234495127a8
Adam Buxton <buxton.adam@gmail.com>
2021-03-22 11:16:07 +0000

    add .gitignore for vagrant

--- install.sh
+++ install.sh
@@ -9,7 +9,11 @@ sudo yum install epel-release -y
 sudo yum update -y
 sudo yum install wget -y
 sudo yum install ansible -y
-curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec
+sudo yum -y install ruby ruby-devel make gcc gcc-c++
+sudo gem install inspec
+sudo gem install bundler
+sudo gem install test-kitchen
+# curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec
 sudo yum install gcc make perl kernel-devel elfutils-libelf-devel kernel-headers bzip2 dkms -y
 sudo yum update kernel-* -y
 export KERN_DIR=/usr/src/kernels/$(uname -r)
